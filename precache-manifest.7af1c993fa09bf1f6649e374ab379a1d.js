c8245f1b93b52e700153a83ad0c2aa87f008b197
Kate Foust <kate.e.foust@gmail.com>
2020-04-19 19:56:48 -0700

    Updates

---
+++ precache-manifest.7af1c993fa09bf1f6649e374ab379a1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "739239ccda18fa5413780ddda986ca98",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "d4f33dae0b640a074bd0",
+    "url": "/clicky-game/static/css/main.cba16e09.chunk.css"
+  },
+  {
+    "revision": "c40e2c27583491ba7818",
+    "url": "/clicky-game/static/js/2.566c9d9f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-game/static/js/2.566c9d9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4f33dae0b640a074bd0",
+    "url": "/clicky-game/static/js/main.2961a10b.chunk.js"
+  },
+  {
+    "revision": "9025a9c5d0326b1623d9",
+    "url": "/clicky-game/static/js/runtime-main.ec6097ba.js"
+  }
+]);
